161d9186afada7518e0942a559173873e75ae56d
铠 <hpk711@126.com>
2022-04-24 04:00:44 +0800

    bing_search

--- bing_search.py
+++ bing_search.py
@@ -3,6 +3,8 @@
 A web search server for ParlAI, including Blenderbot2.
 See README.md
 """
+import bs4
+import html
 import http.server
 import json
 import re
@@ -11,11 +13,12 @@ import urllib.parse
 
 import chardet
 import fire
+import html2text
 from serpapi import GoogleSearch
 # import parlai.agents.rag.retrieve_api
 import rich
 import rich.markup
-
+import requests
 
 print = rich.print
 
@@ -40,6 +43,44 @@ def _parse_host(host: str) -> Tuple[str, int]:
     port = splitted[1] if len(splitted) > 1 else _DEFAULT_PORT
     return hostname, int(port)
 
+def _get_and_parse(url: str) -> Dict[str, str]:
+    """ Download a webpage and parse it. """
+
+    try:
+        resp = requests.get(url, timeout=_REQUESTS_GET_TIMEOUT)
+    except requests.exceptions.RequestException as e:
+        print(f"[!] {e} for url {url}")
+        return None
+    else:
+        resp.encoding = resp.apparent_encoding
+        page = resp.text
+    
+    ###########################################################################
+    # Prepare the title
+    ###########################################################################
+    output_dict = dict(title="", content="", url=url)
+    soup = bs4.BeautifulSoup(page, features="lxml")
+    pre_rendered = soup.find("title")
+    output_dict["title"] = (
+        html.unescape(pre_rendered.renderContents().decode()) if pre_rendered else ""
+    )
+    
+    output_dict["title"] = (
+        output_dict["title"].replace("\n", "").replace("\r", "")
+    )
+
+    ###########################################################################
+    # Prepare the content
+    ###########################################################################
+    text_maker = html2text.HTML2Text()
+    text_maker.ignore_links = True
+    text_maker.ignore_tables = True
+    text_maker.ignore_images = True
+    text_maker.ignore_emphasis = True
+    text_maker.single_line = True
+    output_dict["content"] = html.unescape(text_maker.handle(page).strip())
+
+    return output_dict
 
 class SearchABC(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
@@ -73,23 +114,72 @@ class SearchABC(http.server.BaseHTTPRequestHandler):
         print(f"\n[bold]Received query:[/] {parsed}")
         n = int(parsed["n"])
         q = parsed["q"]
+        loc = parsed.get("cc","US")
 
         # Over query a little bit in case we find useless URLs
         content = []
         # dupe_detection_set = set()
         
-        results = self.search(q, n)["organic_results"]
+        results = self.search(q, n, loc)["organic_results"]
 
         for res in results:
             if (len(content) >= n):
                 break
-            content.append(res)
-            print(res['title'], res['link'])
+            url = res['link']
+
+            maybe_content = _get_and_parse(url)
+
+                        # Check that getting the content didn't fail
+            reason_empty_response = maybe_content is None
+            if not reason_empty_response:
+                reason_content_empty = (
+                    maybe_content["content"] is None
+                    or len(maybe_content["content"]) == 0
+                )
+            else:
+                reason_content_empty = False
+            
+            reasons = dict(
+                reason_empty_response=reason_empty_response,
+                reason_content_empty=reason_content_empty,
+            )
+
+            if not any(reasons.values()):
+                ###############################################################
+                # Log the entry
+                ###############################################################
+                title_str = (
+                    f"`{rich.markup.escape(maybe_content['title'])}`"
+                    if maybe_content["title"]
+                    else "<No Title>"
+                )
+                print(
+                    f" {_STYLE_GOOD}>{_CLOSE_STYLE_GOOD} Result: Title: {title_str}\n"
+                    f"   {rich.markup.escape(maybe_content['url'])}"
+                    # f"Content: {len(maybe_content['content'])}",
+                )
+                content.append(maybe_content)
+                if len(content) >= n:
+                    break
+
+            else:
+                ###############################################################
+                # Log why it failed
+                ###############################################################
+                reason_string = ", ".join(
+                    {
+                        reason_name
+                        for reason_name, whether_failed in reasons.items()
+                        if whether_failed
+                    }
+                )
+                print(f" {_STYLE_SKIP}x{_CLOSE_STYLE_SKIP} Excluding an URL because `{_STYLE_SKIP}{reason_string}{_CLOSE_STYLE_SKIP}`:\n"
+                      f"   {url}") 
 
         ###############################################################
         # Prepare the answer and send it
         ###############################################################
-        # content = content[:n]  
+        content = content[:n]  
         output = json.dumps(dict(response=content)).encode("utf-8")
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -97,7 +187,7 @@ class SearchABC(http.server.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(output)
 
-    def search(self, q: str, n: int) -> Generator[str, None, None]:
+    def search(self, q: str, n: int, loc:str) -> Generator[str, None, None]:
         return NotImplemented(
             "Search is an abstract base class, not meant to be directly "
             "instantiated. You should instantiate a derived class like "
@@ -105,11 +195,11 @@ class SearchABC(http.server.BaseHTTPRequestHandler):
         )
 
 class BingSearch(SearchABC):
-    def search(self, q: str, n: int) -> Generator[str, None, None]:
+    def search(self, q: str, n: int, loc: str) -> Generator[str, None, None]:
         params = {
             "engine": "bing",
             "q": q,
-            "cc": "US",
+            "cc": loc,
             "num": n,
             "api_key": _SECRET_KEY
         }
